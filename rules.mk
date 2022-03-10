0a346d051e99f0db9491088334bec39dfd9372e3
WatsonJay <446725026@qq.com>
2022-03-10 22:52:50 +0800

    Update rules.mk

--- rules.mk
+++ rules.mk
@@ -1,6 +1,9 @@
 # MCU name
 MCU = atmega32u4
 
+# Processor frequency
+F_CPU = 16000000
+
 # Bootloader selection
 BOOTLOADER = caterina
 
@@ -9,10 +12,11 @@ BOOTLOADER = caterina
 #   comment out to disable the options.
 #
 BOOTMAGIC_ENABLE = yes	# Virtual DIP switch configuration(+1000)
-MOUSEKEY_ENABLE ?= yes	# Mouse keys(+4700)
+MOUSEKEY_ENABLE ?= no	# Mouse keys(+4700)
 EXTRAKEY_ENABLE ?= yes	# Audio control and System control(+450)
 CONSOLE_ENABLE ?= no	# Console for debug(+400)
 COMMAND_ENABLE ?= no    # Commands for debug and configuration
 NKRO_ENABLE ?= yes		# USB Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 BACKLIGHT_ENABLE ?= no  # Enable keyboard backlight functionality
 AUDIO_ENABLE ?= no
+
